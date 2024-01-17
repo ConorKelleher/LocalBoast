b55a3b04b97f10196cb715ae56157d0324df01e7
Conor Kelleher <conor95kelleher@hotmail.com>
2024-01-17 21:59:39 +0000

    Correctly export useFooterPadding

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -8,7 +8,7 @@ export { default as useVideoLoader } from "./useVideoLoader"
 export { default as useTwitchChat } from "./useTwitchChat"
 export { default as useSize } from "./useSize"
 export type { UseSizeSize } from "./useSize"
-export { default } from "./useFooterPadding"
+export { default as useFooterPadding } from "./useFooterPadding"
 export { default as useWipe, Shape as UseWipeShape } from "./useWipe"
 export * from "./useTwitchChat"
 
